f796e317a6d7265141610cc6a8ce2b7589cc8510
andr68rus <ashtmb@gmail.com>
2017-04-27 09:31:27 +0300

    Remove oss ifa

--- msm8996.mk
+++ msm8996.mk
@@ -88,9 +88,6 @@ PRODUCT_CHARACTERISTICS := nosdcard
 #    ro.sys.sdcardfs=true
 
 # Alipay / WeChat
-PRODUCT_PACKAGES += \
-    org.ifaa.android.manager
-
 PRODUCT_BOOT_JARS += \
     org.ifaa.android.manager
 
